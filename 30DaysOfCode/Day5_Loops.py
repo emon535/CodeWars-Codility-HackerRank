03721846d0203b25f3e7caeee0627ac4caa93a00
Foysal Ahmed Emon <emon535@gmail.com>
2019-05-04 23:49:48 +0600

    Day 5/30

---
+++ 30DaysOfCode/Day5_Loops.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(10):
+        i=i+1;
+        print(n,"x",i,"=",i*n);
+
