5647fd2fdf6cb80bec9742742e0b2eeddb8abd02
Foysal Ahmed Emon <emon535@gmail.com>
2019-05-04 23:36:49 +0600

    Day 4/30

---
+++ 30DaysOfCode/Day4_Class_vs_Instance.py
@@ -0,0 +1,35 @@
+class Person:
+    age = 0;
+    def __init__(self,initialAge):
+        # Add some more code to run some checks on initialAge
+        if(initialAge<0):
+            print("Age is not valid, setting age to 0.");
+            self.age = 0;
+        else:
+            self.age= initialAge;
+           
+        
+    def amIOld(self):
+        if(age<13):
+            print("You are young.")
+        elif(age >= 13 and age<18):
+            print("You are a teenager.");
+        elif(age>=18):
+            print("You are old.")
+        
+        # Do some computations in here and print out the correct statement to the console
+    def yearPasses(self):
+        
+        global age;
+        age +=1;
+        # Increment the age of the person in here
+
+t = int(input())
+for i in range(0, t):
+    age = int(input())         
+    p = Person(age)  
+    p.amIOld()
+    for j in range(0, 3):
+        p.yearPasses()       
+    p.amIOld()
+    print("")
