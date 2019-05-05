af7c727f44fb91eedd7951a609c18dbcdfc27604
Foysal Ahmed Emon <emon535@gmail.com>
2019-05-05 23:57:59 +0600

    missed code added

--- 30DaysOfCode/Day6_Review.py
+++ 30DaysOfCode/Day6_Review.py
@@ -8,4 +8,6 @@
 
 # Let's Review
 
-
+for N in range(int(input())):
+    S = input()
+    print(S[::2], S[1::2])
--- Interview_Preparation/LinkedList_insert.py
+++ Interview_Preparation/LinkedList_insert.py
@@ -7,3 +7,18 @@
 
 # Insert a node at a specific position in a linked list
 
+def insertNodeAtPosition(head, data, position):
+    node = head;
+    if(position ==0 ):
+        new_node = SinglyLinkedListNode(data)
+        new_node.next = node;
+        return new_node;
+    count = 1;
+    while(count<position and node):
+        count +=1;
+        node = node.next
+    new_node = SinglyLinkedListNode(data);
+    node.next=new_node;
+    new_node.next = node.next;
+   
+    return head;
