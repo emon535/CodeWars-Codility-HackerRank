696a423820c3236ff4e35100207edd30c69a8516
emon535@gmail.com <emon535@gmail.com>
2019-07-09 14:56:06 +0600

    100% Scored in FromJump

--- Codility/TimeComplexity/FrogJump.py
+++ Codility/TimeComplexity/FrogJump.py
@@ -4,13 +4,11 @@ import math
 def solution(X, Y, D):
     count = 0
     distance = Y-X
-
-    if distance%D :
-        print(distance%D)
-        count = math.floor(Y/D)
+    if distance%D ==0 :
+        count = math.floor(distance/D)
     else:
-        count = math.floor(Y/D)+1
+        count = math.floor(distance/D)+1
     return count
 
 
-print(solution(10, 101, 30))
+print(solution(10, 100001, 30))
