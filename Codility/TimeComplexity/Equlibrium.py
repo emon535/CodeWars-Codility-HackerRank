5362296fd1d4814d402c0633d7175e30866ee49a
emon535@gmail.com <emon535@gmail.com>
2019-07-09 14:01:49 +0600

    100% Score on equlibrium/Codility

--- Codility/TimeComplexity/Equlibrium.py
+++ Codility/TimeComplexity/Equlibrium.py
@@ -10,7 +10,7 @@
 #         dif = abs(my_sum-record*2)
 #         differences.append(dif)
 #         minimum = min(differences)
-    
+
 #     # print(minimum, A.index(minimum))
 #     ret = A.index(minimum)
 #     return ret
@@ -20,8 +20,17 @@
 
 
 def solution(A):
+    left = A[0]
+    right = sum(A)-A[0]
+    dif = abs(right-left)
+    for i in range(len(A)-1):
+        left += A[i]
+        right -= A[i]
+        current_dif = abs(left-right)
+        if dif>current_dif:
+            dif = current_dif
+        print(dif)
+    return dif
 
 
-
-
-print(solution[3,1,2,4,3])
+print(solution([3, 1, 2, 4, 3]))
