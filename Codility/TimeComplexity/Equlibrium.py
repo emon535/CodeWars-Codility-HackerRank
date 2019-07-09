1e800f4c2fb83baacf92944b607c893658eeb6f0
emon535@gmail.com <emon535@gmail.com>
2019-07-09 12:59:02 +0600

    Ramson Note Codility Problem solve started.

---
+++ Codility/TimeComplexity/Equlibrium.py
@@ -0,0 +1,27 @@
+
+# def solution(A):
+#     minimum = 0;
+#     record = 0
+#     my_sum = sum(A);
+#     differences = []
+#     # print(my_sum)
+#     for val in A:
+#         record += val
+#         dif = abs(my_sum-record*2)
+#         differences.append(dif)
+#         minimum = min(differences)
+    
+#     # print(minimum, A.index(minimum))
+#     ret = A.index(minimum)
+#     return ret
+
+
+# solution([3,1,2,4,3])
+
+
+def solution(A):
+
+
+
+
+print(solution[3,1,2,4,3])
--- Interview_Preparation_Kit/DictionaryAndHashmap/RansomNote.js
+++ Interview_Preparation_Kit/DictionaryAndHashmap/RansomNote.js
@@ -5,18 +5,21 @@ function checkMagazine(magazine, note) {
     var replicable = true;
     for (var i of magazine) {
         // console.log(map)
-        map[i] = (map[i] || 0) +1;
+        map[i] = (map[i] || 0) + 1;
     }
     for (var i of note) {
- 
-        map[i] = (map[i]||0)-1;
+
+        map[i] = (map[i] || 0) - 1;
         // console.log(map)
     }
     for (var i in map) {
-        if (map[i]<0) {
+        if (map[i] < 0) {
             replicable = false;
             break;
         }
     }
     console.log(replicable ? 'Yes' : 'No');
-   }
+}
+
+
+   // URL : https://www.hackerrank.com/challenges/ctci-ransom-note/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
