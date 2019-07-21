5d1aaa03877a6801dcd822d825dfaba72b11d972
Foysal Ahmed Emon <emon535@gmail.com>
2019-07-21 22:58:28 +0600

    Selection sort

--- Algorithm/Sort/SelectionSort.py
+++ Algorithm/Sort/SelectionSort.py
@@ -1,17 +1,19 @@
-def fineSmallest(arr):
+def findSmallest(arr):
     smallest = arr[0]
     smallest_index = 0
-    for i in range(1,len(arr)):
-        if arr[i] <smallest:
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
             smallest = arr[i]
             smallest_index = i
-    return smallest_index;
+    return smallest_index
+
 
 def selectionSort(arr):
     newArr = []
     for i in range(len(arr)):
-        smallest = fineSmallest(arr)
+        smallest = findSmallest(arr)
         newArr.append(arr.pop(smallest))
-    return newArr;
+    return newArr
 
-print(selectionSort([5,1,3,55,2]))
+
+print(selectionSort([5, 1, 3, 55, 2]))
